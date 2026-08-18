25f5f2a1ea49fa400762ed5a0fe971a4b25a5e1d
smitiwis <sistemas.luis@outlook.com>
2026-08-17 19:31:26 -0500

    [Feat] Conguraciones de las apps para exponer  y consumir

--- mf-detail/vite.config.ts
+++ mf-detail/vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: "mfDetail",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DetailApp": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  server: { port: 3001, strictPort: true, cors: true },
+  preview: { port: 3001, strictPort: true, cors: true },
+  build: {
+    target: "esnext",
+    modulePreload: false,
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
