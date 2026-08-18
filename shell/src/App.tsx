44fb8f64e14d5844426b73f34afd484a29135abf
smitiwis <sistemas.luis@outlook.com>
2026-08-18 10:19:53 -0500

    [Feat] Agregar estilos globales y logica de Dark y light

--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,7 +1,5 @@
+import { AppRouter } from "./routes/AppRouter";
+
 export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Shell App</h1>
-    </div>
-  );
+  return <AppRouter />;
 }
